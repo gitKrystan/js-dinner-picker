8675db38fd53b6dfef8a34d26649b80421efc016
Jeffrey Ruder and Krystan Menne <kmenne@gmail.com>
2016-02-17 10:32:39 -0800

    update initial map interface

---
+++ js/initial-map-interface.js
@@ -0,0 +1,36 @@
+var INITIAL_MAP_LAT = 32.7767;
+var INITIAL_MAP_LNG = 96.7970;
+
+var initMap = function($mapDiv) {
+  var map = new google.maps.Map($mapDiv[0], {
+    center: {lat: INITIAL_MAP_LAT, lng: INITIAL_MAP_LNG},
+    zoom: 15
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+var handleLocationError = function(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
